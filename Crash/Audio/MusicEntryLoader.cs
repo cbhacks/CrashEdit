f7d5c62ef9632b86fd6ec8a1fd996c4388bf2bb5
U-VAMOOLA\Administrator <Administrator@vamoola.(none)>
2013-03-02 12:33:38 +0000

    Add a constraint to MusicEntryLoader

--- Crash/Audio/MusicEntryLoader.cs
+++ Crash/Audio/MusicEntryLoader.cs
@@ -15,6 +15,12 @@ namespace Crash.Audio
             }
             byte[] unknown1 = items[0];
             int seqcount = BitConv.FromWord(unknown1,0);
+            if (BitConv.FromWord(unknown1,24) != 0x6396347F)
+                throw new LoadException();
+            if (BitConv.FromWord(unknown1,28) != 0x6396347F)
+                throw new LoadException();
+            if (BitConv.FromWord(unknown1,32) != 0x6396347F)
+                throw new LoadException();
             byte[] vh = items[1];
             SEP sep = SEP.Load(items[2],seqcount);
             return new MusicEntry(unknown1,vh,sep,unknown);
