cbecd913b513cdf1f8eb0a29cbcfa80b714a5505
U-VAMOOLA\Administrator <Administrator@vamoola.(none)>
2013-03-18 15:21:58 +0000

    Remove obsolete BitConv functions

--- Crash/BitConv.cs
+++ Crash/BitConv.cs
@@ -4,42 +4,6 @@ namespace Crash
 {
     public static class BitConv
     {
-        [Obsolete]
-        public static short FromHalf(byte[] str,int offset)
-        {
-            return FromShortLE(str,offset);
-        }
-
-        [Obsolete]
-        public static int FromWord(byte[] str,int offset)
-        {
-            return FromIntLE(str,offset);
-        }
-
-        [Obsolete]
-        public static short FromShortLE(byte[] str,int offset)
-        {
-            return FromInt16(str,offset);
-        }
-
-        [Obsolete]
-        public static int FromIntLE(byte[] str,int offset)
-        {
-            return FromInt32(str,offset);
-        }
-
-        [Obsolete]
-        public static short FromShortBE(byte[] str,int offset)
-        {
-            return BEBitConv.FromInt16(str,offset);
-        }
-
-        [Obsolete]
-        public static int FromIntBE(byte[] str,int offset)
-        {
-            return BEBitConv.FromInt32(str,offset);
-        }
-
         public static short FromInt16(byte[] str,int offset)
         {
             if (str == null)
@@ -68,42 +32,6 @@ namespace Crash
             result |= str[offset + 2] << 8 * 2;
             result |= str[offset + 3] << 8 * 3;
             return result;
-        }
-
-        [Obsolete]
-        public static void ToHalf(byte[] str,int offset,short value)
-        {
-            ToShortLE(str,offset,value);
-        }
-
-        [Obsolete]
-        public static void ToWord(byte[] str,int offset,int value)
-        {
-            ToIntLE(str,offset,value);
-        }
-
-        [Obsolete]
-        public static void ToShortLE(byte[] str,int offset,short value)
-        {
-            ToInt16(str,offset,value);
-        }
-
-        [Obsolete]
-        public static void ToIntLE(byte[] str,int offset,int value)
-        {
-            ToInt32(str,offset,value);
-        }
-
-        [Obsolete]
-        public static void ToShortBE(byte[] str,int offset,short value)
-        {
-            BEBitConv.ToInt16(str,offset,value);
-        }
-
-        [Obsolete]
-        public static void ToIntBE(byte[] str,int offset,int value)
-        {
-            BEBitConv.ToInt32(str,offset,value);
         }
 
         public static void ToInt16(byte[] str,int offset,short value)
