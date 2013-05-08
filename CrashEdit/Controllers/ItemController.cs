46829d94c98e032a48f783c4c1e100f4cdcd89f4
U-VAMOOLA\Administrator <Administrator@vamoola.(none)>
2013-05-08 17:53:52 +0000

    Add replace and delete options for items in mystery entries

--- CrashEdit/Controllers/ItemController.cs
+++ CrashEdit/Controllers/ItemController.cs
@@ -14,6 +14,11 @@ namespace CrashEdit
             Node.Text = "Item";
             Node.ImageKey = "item";
             Node.SelectedImageKey = "item";
+            if (mysteryentrycontroller != null)
+            {
+                AddMenu("Replace Item",Menu_Replace_Item);
+                AddMenu("Delete Item",Menu_Delete_Item);
+            }
         }
 
         protected override Control CreateEditor()
@@ -29,6 +34,24 @@ namespace CrashEdit
         public byte[] Item
         {
             get { return item; }
+        }
+
+        private void Menu_Replace_Item()
+        {
+            int i = mysteryentrycontroller.MysteryEntry.Items.IndexOf(item);
+            byte[] data = FileUtil.OpenFile(FileUtil.AnyFilter);
+            if (data != null)
+            {
+                item = data;
+                mysteryentrycontroller.MysteryEntry.Items[i] = data;
+                InvalidateEditor();
+            }
+        }
+
+        private void Menu_Delete_Item()
+        {
+            mysteryentrycontroller.MysteryEntry.Items.Remove(item);
+            Dispose();
         }
     }
 }
