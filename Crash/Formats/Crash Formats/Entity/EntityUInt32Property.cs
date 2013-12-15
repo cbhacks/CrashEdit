883f7de735408fdf3e9cff8bc9ce88085c30df45
U-VAMOOLA\Administrator <Administrator@vamoola.(none)>
2013-12-15 13:21:15 +0000

    Add EntityUInt32Property

---
+++ Crash/Formats/Crash Formats/Entity/EntityUInt32Property.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace Crash
+{
+    public sealed class EntityUInt32Property : EntityBasicProperty<uint>
+    {
+        public EntityUInt32Property()
+        {
+        }
+
+        public EntityUInt32Property(IEnumerable<EntityPropertyRow<uint>> rows) : base(rows)
+        {
+        }
+
+        public override byte Type
+        {
+            get { return 3; }
+        }
+
+        public override byte ElementSize
+        {
+            get { return 4; }
+        }
+
+        protected override void SaveElement(byte[] data,uint value)
+        {
+            BitConv.ToInt32(data,0,(int)value);
+        }
+    }
+}
---
+++ Crash/Formats/Crash Formats/Entity/EntityUInt32PropertyLoader.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace Crash
+{
+    [EntityPropertyType(3)]
+    public sealed class EntityUInt32PropertyLoader : EntityBasicPropertyLoader<uint>
+    {
+        protected override byte ElementSize
+        {
+            get { return 4; }
+        }
+
+        protected override uint LoadElement(byte[] data)
+        {
+            return (uint)BitConv.FromInt32(data,0);
+        }
+        
+        protected override EntityProperty Load(IEnumerable<EntityPropertyRow<uint>> rows)
+        {
+            return new EntityUInt32Property(rows);
+        }
+    }
+}
