b0eff61576fbc60c3ff5ba07b9782dd334f4aa4b
ManDude <mandude@users.noreply.github.com>
2019-11-01 07:06:38 +0000

    Update WavebankEntry.cs

--- Crash/Formats/Crash Formats/Wavebank/WavebankEntry.cs
+++ Crash/Formats/Crash Formats/Wavebank/WavebankEntry.cs
@@ -4,15 +4,10 @@ namespace Crash
 {
     public sealed class WavebankEntry : Entry
     {
-        private int id;
-        private SampleSet samples;
-
         public WavebankEntry(int id,SampleSet samples,int eid,int size) : base(eid,size)
         {
-            if (samples == null)
-                throw new ArgumentNullException("samples");
-            this.id = id;
-            this.samples = samples;
+            ID = id;
+            Samples = samples ?? throw new ArgumentNullException("samples");
         }
 
         public override int Type
@@ -20,21 +15,15 @@ namespace Crash
             get { return 14; }
         }
 
-        public int ID
-        {
-            get { return id; }
-        }
+        public int ID { get; }
 
-        public SampleSet Samples
-        {
-            get { return samples; }
-        }
+        public SampleSet Samples { get; }
 
         public override UnprocessedEntry Unprocess()
         {
             byte[] info = new byte [8];
-            byte[] data = samples.Save();
-            BitConv.ToInt32(info,0,id);
+            byte[] data = Samples.Save();
+            BitConv.ToInt32(info,0,ID);
             BitConv.ToInt32(info,4,data.Length);
             byte[][] items = new byte [2][];
             items[0] = info;
