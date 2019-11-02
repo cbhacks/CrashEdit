12f1b8b912ecf7a1af5529aa5a9cfb59095e7fd4
ManDude <mandude@users.noreply.github.com>
2019-11-02 05:27:17 +0000

    Update OldScenery/ProtoScenery

--- Crash/Formats/Crash Formats/Scenery/OldSceneryPolygon.cs
+++ Crash/Formats/Crash Formats/Scenery/OldSceneryPolygon.cs
@@ -21,13 +21,6 @@ namespace Crash
             return new OldSceneryPolygon(vertexa,vertexb,vertexc,unknown1,unknown2,unknown3);
         }
 
-        private int vertexa;
-        private int vertexb;
-        private int vertexc;
-        private int unknown1;
-        private byte unknown2;
-        private byte unknown3;
-
         public OldSceneryPolygon(int vertexa,int vertexb,int vertexc,int unknown1,byte unknown2,byte unknown3)
         {
             if (vertexa < 0 || vertexa > 0xFFF)
@@ -38,54 +31,31 @@ namespace Crash
                 throw new ArgumentOutOfRangeException("vertexc");
             if (unknown1 < 0 || unknown1 > 0xFFF)
                 throw new ArgumentOutOfRangeException("unknown1");
-            this.vertexa = vertexa;
-            this.vertexb = vertexb;
-            this.vertexc = vertexc;
-            this.unknown1 = unknown1;
-            this.unknown2 = unknown2;
-            this.unknown3 = unknown3;
+            VertexA = vertexa;
+            VertexB = vertexb;
+            VertexC = vertexc;
+            Unknown1 = unknown1;
+            Unknown2 = unknown2;
+            Unknown3 = unknown3;
         }
 
-        public int VertexA
-        {
-            get { return vertexa; }
-        }
-
-        public int VertexB
-        {
-            get { return vertexb; }
-        }
-
-        public int VertexC
-        {
-            get { return vertexc; }
-        }
-
-        public int Unknown1
-        {
-            get { return unknown1; }
-        }
-
-        public byte Unknown2
-        {
-            get { return unknown2; }
-        }
-
-        public byte Unknown3
-        {
-            get { return unknown3; }
-        }
+        public int VertexA { get; }
+        public int VertexB { get; }
+        public int VertexC { get; }
+        public int Unknown1 { get; }
+        public byte Unknown2 { get; }
+        public byte Unknown3 { get; }
 
         public byte[] Save()
         {
             int worda = 0;
             int wordb = 0;
-            worda |= vertexa << 20;
-            wordb |= vertexb << 8;
-            wordb |= vertexc << 20;
-            worda |= unknown1 << 8;
-            worda |= unknown2;
-            wordb |= unknown3;
+            worda |= VertexA << 20;
+            wordb |= VertexB << 8;
+            wordb |= VertexC << 20;
+            worda |= Unknown1 << 8;
+            worda |= Unknown2;
+            wordb |= Unknown3;
             byte[] data = new byte [8];
             BitConv.ToInt32(data,0,worda);
             BitConv.ToInt32(data,4,wordb);
