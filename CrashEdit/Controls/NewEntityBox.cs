b718ea80cadc7a7ca842f5f34c29c50c689ff675
ManDude <mandude@users.noreply.github.com>
2019-11-02 02:51:08 +0000

    Remove unused function from entity boxes

--- CrashEdit/Controls/NewEntityBox.cs
+++ CrashEdit/Controls/NewEntityBox.cs
@@ -53,13 +53,6 @@ namespace CrashEdit
             controller.InvalidateNode();
         }
 
-        public static string Reverse(string str)
-        {
-            char[] charArray = str.ToCharArray();
-            Array.Reverse(charArray);
-            return new string(charArray);
-        }
-
         private void UpdateName()
         {
             if (entity.Name != null)
