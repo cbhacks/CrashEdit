db66e78765e00550c3b111e82f0589d0be07eb07
ManDude <mandude@users.noreply.github.com>
2019-11-02 15:02:24 +0000

    Update TextureChunkController.cs

--- CrashEdit/Controllers/TextureChunkController.cs
+++ CrashEdit/Controllers/TextureChunkController.cs
@@ -6,18 +6,16 @@ namespace CrashEdit
 {
     public sealed class TextureChunkController : ChunkController
     {
-        private TextureChunk texturechunk;
-
         public TextureChunkController(NSFController nsfcontroller,TextureChunk texturechunk) : base(nsfcontroller,texturechunk)
         {
-            this.texturechunk = texturechunk;
+            TextureChunk = texturechunk;
             AddMenu("Recalculate Checksum",Menu_Recalculate_Checksum);
             InvalidateNode();
         }
 
         public override void InvalidateNode()
         {
-            Node.Text = string.Format("Texture Chunk {1} ({0})",Entry.EIDToEName(texturechunk.EID),NSFController.chunkid);
+            Node.Text = string.Format("Texture Chunk {1} ({0})",Entry.EIDToEName(TextureChunk.EID),NSFController.chunkid);
             Node.ImageKey = "image";
             Node.SelectedImageKey = "image";
         }
@@ -27,24 +25,21 @@ namespace CrashEdit
             // Hack for Mono so it doesn't crash.
             if (Type.GetType("Mono.Runtime") != null)
                 return base.CreateEditor();
-            return new TextureChunkBox(texturechunk);
+            return new TextureChunkBox(TextureChunk);
         }
 
-        public TextureChunk TextureChunk
-        {
-            get { return texturechunk; }
-        }
+        public TextureChunk TextureChunk { get; }
 
         private void Menu_Recalculate_Checksum()
         {
-            int current_checksum = BitConv.FromInt32(texturechunk.Data, 12);
-            int correct_checksum = Chunk.CalculateChecksum(texturechunk.Data);
+            int current_checksum = BitConv.FromInt32(TextureChunk.Data, 12);
+            int correct_checksum = Chunk.CalculateChecksum(TextureChunk.Data);
             if (current_checksum == correct_checksum)
             {
                 MessageBox.Show("Checksum was already correct.");
                 return;
             }
-            BitConv.ToInt32(texturechunk.Data, 12, correct_checksum);
+            BitConv.ToInt32(TextureChunk.Data, 12, correct_checksum);
             MessageBox.Show("Checksum was incorrect and has been corrected.");
         }
     }
