db746918c45b81fc5ce94ba5af1452b7ca833f37
ManDude <mandude@users.noreply.github.com>
2019-11-11 07:10:34 +0000

    Better handling of transformed Crash 2 model polys

---
+++ Crash/Formats/Crash Formats/Models/ModelTransformedTriangle.cs
@@ -0,0 +1,20 @@
+﻿namespace Crash
+{
+    public struct ModelTransformedTriangle
+    {
+        public ModelTransformedTriangle(int v1,int v2,int v3,int c1,int c2,int c3,int tex,int type,int subtype)
+        {
+            Vertex = new int[3] { v1, v2, v3 };
+            Color = new int[3] { c1, c2, c3 };
+            Tex = tex;
+            Type = type;
+            Subtype = subtype;
+        }
+
+        public int[] Vertex { get; }
+        public int[] Color { get; }
+        public int Tex { get; }
+        public int Type { get; }
+        public int Subtype { get; }
+    }
+}
