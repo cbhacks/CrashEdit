17ebc5e14338873fe885e01efaf61f3318c0adae
ManDude <mandude@users.noreply.github.com>
2019-12-08 05:42:58 +0000

    "Fix" RET and BEQZ formats (warp room)

--- Crash/Formats/Crash Formats/GOOL/GOOL Instructions/Ret.cs
+++ Crash/Formats/Crash Formats/GOOL/GOOL Instructions/Ret.cs
@@ -7,7 +7,7 @@
         public Ret(int value,GOOLEntry gool) : base(value,gool) { }
 
         public override string Name => "RET";
-        public override string Format => "00000000000000 101001 00 01";
+        public override string Format => "---------- 0000 ------ ----";
         public override string Comment => $"return";
     }
 }
