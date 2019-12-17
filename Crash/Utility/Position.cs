fcd754e6653a53b38d381720d52c632189bed419
ManDude <mandude@users.noreply.github.com>
2019-12-17 03:37:29 +0000

    Give Position some basic operators and useful things

--- Crash/Utility/Position.cs
+++ Crash/Utility/Position.cs
@@ -2,6 +2,8 @@ namespace Crash
 {
     public struct Position : IPosition
     {
+        public static readonly Position Unit = new Position(1,1,1);
+
         public Position(double x,double y,double z)
         {
             X = x;
@@ -9,8 +11,24 @@ namespace Crash
             Z = z;
         }
 
+        public Position(IPosition pos)
+        {
+            X = pos.X;
+            Y = pos.Y;
+            Z = pos.Z;
+        }
+
         public double X { get; }
         public double Y { get; }
         public double Z { get; }
+
+        public static Position operator +(Position a, Position b) => new Position(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        public static Position operator -(Position a, Position b) => new Position(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        public static Position operator *(Position a, Position b) => new Position(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+        public static Position operator /(Position a, Position b) => new Position(a.X / b.X, a.Y / b.Y, a.Z / b.Z);
+        public static Position operator *(Position a, double b) => new Position(a.X * b, a.Y * b, a.Z * b);
+        public static Position operator *(double b, Position a) => new Position(a.X * b, a.Y * b, a.Z * b);
+        public static Position operator /(Position a, double b) => new Position(a.X / b, a.Y / b, a.Z / b);
+        public static Position operator /(double b, Position a) => new Position(a.X / b, a.Y / b, a.Z / b);
     }
 }
