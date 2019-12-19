3f22bf80b6f97680254b47594aad879252e17f7d
ManDude <mandude@users.noreply.github.com>
2019-12-19 01:00:19 +0000

    Give Crash/CrashUI/CrashEdit an assemblyinfo file

---
+++ Crash/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CrashEdit 'Crash' library")]
+[assembly: AssemblyDescription("Unofficial Crash Bandicoot 1/2/3 Level Editor library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("cbhacks")]
+[assembly: AssemblyProduct("Crash.Properties")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("48b4e0e7-53d3-4882-83cd-ebf7dfce670f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0.0.30")]
+[assembly: AssemblyFileVersion("2.0.0.30")]
---
+++ CrashEdit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CrashEdit [deprecate]")]
+[assembly: AssemblyDescription("Unofficial Crash Bandicoot 1/2/3 Level Editor")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("cbhacks")]
+[assembly: AssemblyProduct("CrashEdit.Properties")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("63792b82-e183-43c9-a525-11dfae605f9f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0.0.30")]
+[assembly: AssemblyFileVersion("2.0.0.30")]
+[assembly: NeutralResourcesLanguage("en")]
+
