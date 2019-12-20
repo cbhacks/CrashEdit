17b63d74de25026272850a64f214d27c888578e3
ManDude <mandude@users.noreply.github.com>
2019-12-20 01:47:26 +0000

    Increase version numbers

--- Crash/Properties/AssemblyInfo.cs
+++ Crash/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.33.0")]
-[assembly: AssemblyFileVersion("0.2.33.0")]
+[assembly: AssemblyVersion("0.2.34.0")]
+[assembly: AssemblyFileVersion("0.2.34.0")]
--- CrashEdit/Properties/AssemblyInfo.cs
+++ CrashEdit/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.33.0")]
-[assembly: AssemblyFileVersion("0.2.33.0")]
+[assembly: AssemblyVersion("0.2.34.0")]
+[assembly: AssemblyFileVersion("0.2.34.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
