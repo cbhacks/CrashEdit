9fa329a8ccc3c457d798e5911d52a60a494d0313
ManDude <mandude@users.noreply.github.com>
2019-12-24 05:00:49 +0000

    Update ThreeDimensionalViewer.cs

--- CrashEdit/Controls/ThreeDimensionalViewer.cs
+++ CrashEdit/Controls/ThreeDimensionalViewer.cs
@@ -272,7 +272,7 @@ namespace CrashEdit
             GL.ClearColor(0.025f,0.025f,0.025f,1);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.MatrixMode(MatrixMode.Projection);
-            var proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver3,(float)Width/Height,200,2000000);
+            var proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver3,(float)Width/Height,300,2000000);
             GL.LoadMatrix(ref proj);
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadIdentity();
