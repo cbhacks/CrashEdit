d067c4ca18271cf13c98c09c994d9c351cf12de7
ManDude <7569514+ManDude@users.noreply.github.com>
2019-12-26 05:44:03 +0000

    Add GOOL convertexport

--- CrashEdit/Controllers/GOOL/GOOLEntryController.cs
+++ CrashEdit/Controllers/GOOL/GOOLEntryController.cs
@@ -1,4 +1,6 @@
 using Crash;
+using Crash.GOOLIns;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace CrashEdit
@@ -9,6 +11,8 @@ namespace CrashEdit
         {
             GOOLEntry = goolentry;
             InvalidateNode();
+            if (GOOLEntry.Version == GOOLVersion.Version0)
+                AddMenu("Export as Crash 1 GOOL", Menu_ExportAsC1);
         }
 
         public override void InvalidateNode()
@@ -35,5 +39,25 @@ namespace CrashEdit
         }
 
         public GOOLEntry GOOLEntry { get; }
+
+        public void Menu_ExportAsC1()
+        {
+            byte[] newinstructions = new byte [GOOLEntry.Instructions.Count * 4];
+            for (int i = 0; i < GOOLEntry.Instructions.Count; ++i)
+            {
+                if (GOOLEntry.Instructions[i] is Cfl_95 cfl)
+                {
+                    int newval = cfl.Save();
+                    newval = (newval & ~0x3FF) | cfl.Args['I'].Value & 0x3FF;
+                    newval = (newval & ~0b11110000000000) | (cfl.Args['V'].Value << 10);
+                    BitConv.ToInt32(newinstructions,i*4, newval);
+                }
+                else
+                    BitConv.ToInt32(newinstructions,i*4,GOOLEntry.Instructions[i].Save());
+            }
+            GOOLEntry newgool = new GOOLEntry(GOOLVersion.Version1,GOOLEntry.Header,newinstructions,GOOLEntry.Data,GOOLEntry.StateMap,GOOLEntry.StateDescriptors,GOOLEntry.Anims,GOOLEntry.EID);
+            //FileUtil.SaveFile(newgool.Save(), FileFilters.NSEntry, FileFilters.Any);
+            System.IO.File.WriteAllBytes(@"C:\GameData\Crash 1\Modded\PROTO\S1\NEW-" + newgool.EName + ".nsentry", newgool.Save());
+        }
     }
 }
