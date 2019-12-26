0012848e0f5333b6beca530466ef20eac5f937e2
ManDude <7569514+ManDude@users.noreply.github.com>
2019-12-26 02:23:56 +0000

    update version numbers

--- Crash.UI/Properties/AssemblyInfo.cs
+++ Crash.UI/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.40.0")]
-[assembly: AssemblyFileVersion("0.2.40.0")]
+[assembly: AssemblyVersion("0.2.41.0")]
+[assembly: AssemblyFileVersion("0.2.41.0")]
