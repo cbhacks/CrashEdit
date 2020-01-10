9d8a0b0b33fc9505e3940b19f7841fb4ff1183bc
ManDude <7569514+ManDude@users.noreply.github.com>
2020-01-10 23:30:14 +0000

    Render Map as a single image instead of a trillion smaller ones (massive performance boost)

--- CrashEdit/Controls/MapEntryViewer.cs
+++ CrashEdit/Controls/MapEntryViewer.cs
@@ -21,7 +21,18 @@ namespace CrashEdit
                 }
             }
             int imagecount = 0;
+            for (int i = 0, s = BitConv.FromInt32(ec.MapEntry.Header,0); i < s; ++i)
+            {
+                ImageEntry imag = ec.EntryChunkController.NSFController.NSF.FindEID<ImageEntry>(BitConv.FromInt32(ec.MapEntry.Header,0x1B0+i*4));
+                imagecount += imag.Items.Count;
+            }
             int size = BitConv.FromInt32(ec.MapEntry.Header,0x4);
+            Bitmap bitmap = new Bitmap((int)(System.Math.Ceiling(imagecount/(double)size)*16),size*16,PixelFormat.Format16bppArgb1555);
+            Rectangle brect = new Rectangle(Point.Empty,bitmap.Size);
+            BitmapData bdata = bitmap.LockBits(brect, ImageLockMode.WriteOnly,PixelFormat.Format16bppArgb1555);
+            imagecount = 0;
+            try
+            {
             for (int i = 0, s = BitConv.FromInt32(ec.MapEntry.Header,0); i < s; ++i)
             {
                 ImageEntry imag = ec.EntryChunkController.NSFController.NSF.FindEID<ImageEntry>(BitConv.FromInt32(ec.MapEntry.Header,0x1B0+i*4));
@@ -33,15 +44,9 @@ namespace CrashEdit
                         if (palette == -2) // null
                         {
                             ++imagecount;
-                            continue;
                         }
                         else if (palette == -1) // 16bpp
                         {
-                            Bitmap bitmap = new Bitmap(16,16,PixelFormat.Format16bppArgb1555);
-                            Rectangle brect = new Rectangle(Point.Empty,bitmap.Size);
-                            BitmapData bdata = bitmap.LockBits(brect,ImageLockMode.WriteOnly,PixelFormat.Format16bppArgb1555);
-                            try
-                            {
                                 for (int y = 0; y < 16; ++y)
                                 {
                                     for (int x = 0; x < 16; ++x)
@@ -49,30 +54,13 @@ namespace CrashEdit
                                         short color = BitConv.FromInt16(frame,(y*16+x) * 2 + 4);
                                         PixelConv.Unpack1555(color,out byte a,out byte b,out byte g,out byte r);
                                         color = PixelConv.Pack1555(1,r,g,b);
-                                        System.Runtime.InteropServices.Marshal.WriteInt16(bdata.Scan0,x*2 + y*bdata.Stride,color);
+                                        System.Runtime.InteropServices.Marshal.WriteInt16(bdata.Scan0,(imagecount/size*16+x)*2 + (imagecount%size*16+y)*bdata.Stride,color);
                                     }
                                 }
-                            }
-                            finally
-                            {
-                                bitmap.UnlockBits(bdata);
-                            }
-                            PictureBox picture = new PictureBox
-                            {
-                                Location = new Point(imagecount / size * 16,imagecount % size * 16),
-                                Size = new Size(16,16),
-                                Image = bitmap
-                            };
-                            Controls.Add(picture);
                             ++imagecount;
                         }
                         else // 8bpp
                         {
-                            Bitmap bitmap = new Bitmap(16,16,PixelFormat.Format16bppArgb1555);
-                            Rectangle brect = new Rectangle(Point.Empty,bitmap.Size);
-                            BitmapData bdata = bitmap.LockBits(brect,ImageLockMode.WriteOnly,PixelFormat.Format16bppArgb1555);
-                            try
-                            {
                                 for (int y = 0; y < 16; ++y)
                                 {
                                     for (int x = 0; x < 16; ++x)
@@ -80,26 +68,25 @@ namespace CrashEdit
                                         short color = BitConv.FromInt16(palettes[palette],frame[(y*16+x) + 4]*2);
                                         PixelConv.Unpack1555(color,out byte a,out byte b,out byte g,out byte r);
                                         color = PixelConv.Pack1555(1,r,g,b);
-                                        System.Runtime.InteropServices.Marshal.WriteInt16(bdata.Scan0,x*2 + y*bdata.Stride,color);
+                                        System.Runtime.InteropServices.Marshal.WriteInt16(bdata.Scan0,(imagecount/size*16+x)*2 + (imagecount%size*16+y)*bdata.Stride,color);
                                     }
                                 }
-                            }
-                            finally
-                            {
-                                bitmap.UnlockBits(bdata);
-                            }
-                            PictureBox picture = new PictureBox
-                            {
-                                Location = new Point(imagecount / size * 16,imagecount % size * 16),
-                                Size = new Size(16,16),
-                                Image = bitmap
-                            };
-                            Controls.Add(picture);
                             ++imagecount;
                         }
                     }
                 }
             }
+            }
+            finally
+            {
+                bitmap.UnlockBits(bdata);
+            }
+            PictureBox picture = new PictureBox
+            {
+                Size = bitmap.Size,
+                Image = bitmap
+            };
+            Controls.Add(picture);
             AutoScroll = true;
         }
     }
