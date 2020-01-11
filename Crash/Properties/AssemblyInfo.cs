5334ba7486dff13f50700df90c7c96a5ceebf27b
ManDude <7569514+ManDude@users.noreply.github.com>
2020-01-11 05:51:40 +0000

    Fix Crash 1 proto wgeo convertexport

--- Crash/Properties/AssemblyInfo.cs
+++ Crash/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.68.0")]
-[assembly: AssemblyFileVersion("0.2.68.0")]
+[assembly: AssemblyVersion("0.2.68.1")]
+[assembly: AssemblyFileVersion("0.2.68.1")]
--- CrashEdit/Controllers/Scenery/ProtoSceneryEntryController.cs
+++ CrashEdit/Controllers/Scenery/ProtoSceneryEntryController.cs
@@ -61,17 +61,73 @@ namespace CrashEdit
         private void Menu_ExportAsC1()
         {
             List<OldSceneryPolygon> polygons = new List<OldSceneryPolygon>();
+            List<OldSceneryVertex> vertices = new List<OldSceneryVertex>();
             foreach (var protop in ProtoSceneryEntry.Polygons)
             {
-                polygons.Add(new OldSceneryPolygon(protop.VertexA,protop.VertexB,protop.VertexC,protop.Texture,(byte)protop.Page,0,0));
+                byte r,g,b;
+                OldModelStruct str = ProtoSceneryEntry.Structs[protop.Texture];
+                if (str is OldSceneryTexture tex)
+                {
+                    r = tex.R;
+                    g = tex.G;
+                    b = tex.B;
+                }
+                else if (str is OldSceneryColor col)
+                {
+                    r = col.R;
+                    g = col.G;
+                    b = col.B;
+                }
+                else
+                {
+                    r = 0x7F;
+                    g = 0x7F;
+                    b = 0x7F;
+                }
+                polygons.Add(new OldSceneryPolygon(polygons.Count*3,polygons.Count*3+1,polygons.Count*3+2,protop.Texture,(byte)protop.Page,0,0));
+                var va = ProtoSceneryEntry.Vertices[protop.VertexA];
+                var vb = ProtoSceneryEntry.Vertices[protop.VertexB];
+                var vc = ProtoSceneryEntry.Vertices[protop.VertexC];
+                vertices.Add(new OldSceneryVertex((short)((short)(va.X/8.0)*8),(short)((short)(va.Y/8.0)*8),(short)((short)(va.Z/8.0)*8),r,g,b,false));
+                vertices.Add(new OldSceneryVertex((short)((short)(vb.X/8.0)*8),(short)((short)(vb.Y/8.0)*8),(short)((short)(vb.Z/8.0)*8),r,g,b,false));
+                vertices.Add(new OldSceneryVertex((short)((short)(vc.X/8.0)*8),(short)((short)(vc.Y/8.0)*8),(short)((short)(vc.Z/8.0)*8),r,g,b,false));
             }
-            List<OldSceneryVertex> vertices = new List<OldSceneryVertex>();
-            foreach (var protov in ProtoSceneryEntry.Vertices)
+            for (int i = 0; i < vertices.Count; ++i)
             {
-                vertices.Add(new OldSceneryVertex((short)((short)(protov.X/8.0)*8),(short)((short)(protov.Y/8.0)*8),(short)((short)(protov.Z/8.0)*8),0x7F,0x7F,0x7F,false));
+                OldSceneryVertex basevertex = vertices[i];
+                for (int j = vertices.Count-1; j > i; --j)
+                {
+                    OldSceneryVertex testvertex = vertices[j];
+                    if (basevertex.Red == testvertex.Red &&
+                        basevertex.Green == testvertex.Green &&
+                        basevertex.Blue == testvertex.Blue &&
+                        basevertex.X == testvertex.X &&
+                        basevertex.Y == testvertex.Y &&
+                        basevertex.Z == testvertex.Z)
+                    {
+                        for (int k = 0; k < polygons.Count; ++k)
+                        {
+                            var poly = polygons[k];
+                            if (poly.VertexA == j)
+                                poly.VertexA = i;
+                            else if (poly.VertexA > j)
+                                --poly.VertexA;
+                            if (poly.VertexB == j)
+                                poly.VertexB = i;
+                            else if (poly.VertexB > j)
+                                --poly.VertexB;
+                            if (poly.VertexC == j)
+                                poly.VertexC = i;
+                            else if (poly.VertexC > j)
+                                --poly.VertexC;
+                            polygons[k] = poly;
+                        }
+                        vertices.RemoveAt(j);
+                    }
+                }
             }
-            List<OldModelStruct> structs = new List<OldModelStruct>(); // TODO: implement
-            OldSceneryEntry newworld = new OldSceneryEntry(ProtoSceneryEntry.Info,polygons,vertices,structs,null,ProtoSceneryEntry.EID);
+            OldSceneryEntry newworld = new OldSceneryEntry(ProtoSceneryEntry.Info,polygons,vertices,ProtoSceneryEntry.Structs,null,ProtoSceneryEntry.EID);
+            BitConv.ToInt32(newworld.Info,0x10,vertices.Count);
             FileUtil.SaveFile(newworld.Save(), FileFilters.NSEntry, FileFilters.Any);
         }
     }
