ab4a3c6608a24e4c7c080875aadcb3147afdc2c8
ManDude <7569514+ManDude@users.noreply.github.com>
2020-01-20 01:51:29 +0000

    Update AssemblyInfo.cs

--- CrashEdit/Properties/AssemblyInfo.cs
+++ CrashEdit/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.79")]
-[assembly: AssemblyFileVersion("0.2.79")]
+[assembly: AssemblyVersion("0.2.79.1")]
+[assembly: AssemblyFileVersion("0.2.79.1")]
 [assembly: NeutralResourcesLanguage("en")]
 
