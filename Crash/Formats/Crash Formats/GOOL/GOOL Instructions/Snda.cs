c216fb6871a197ad39c0dc9a12304042a399d85a
ManDude <7569514+ManDude@users.noreply.github.com>
2020-02-17 19:06:42 +0000

    Fix sound instruction details

--- Crash/Formats/Crash Formats/GOOL/GOOL Instructions/Snda.cs
+++ Crash/Formats/Crash Formats/GOOL/GOOL Instructions/Snda.cs
@@ -1,17 +1,30 @@
 ﻿namespace Crash.GOOLIns
 {
-    [GOOLInstruction(140,GameVersion.Crash1)]
-    [GOOLInstruction(140,GameVersion.Crash1Beta1995)]
-    [GOOLInstruction(140,GameVersion.Crash1BetaMAR08)]
-    [GOOLInstruction(140,GameVersion.Crash1BetaMAY11)]
-    [GOOLInstruction(65,GameVersion.Crash2)]
-    [GOOLInstruction(65,GameVersion.Crash3)]
+    [GOOLInstruction(141,GameVersion.Crash1)]
+    [GOOLInstruction(141,GameVersion.Crash1Beta1995)]
+    [GOOLInstruction(141,GameVersion.Crash1BetaMAR08)]
+    [GOOLInstruction(141,GameVersion.Crash1BetaMAY11)]
+    [GOOLInstruction(66,GameVersion.Crash2)]
+    [GOOLInstruction(66,GameVersion.Crash3)]
     public sealed class Snda : GOOLInstruction
     {
         public Snda(int value,GOOLEntry gool) : base(value,gool) { }
 
         public override string Name => "SNDA";
-        public override string Format => DefaultFormat;
-        public override string Comment => string.Empty;
+        public override string Format => "[SSSSSSSSSSSS] (RRRRRR) FF TTTT";
+        public override string Comment
+        {
+            get
+            {
+                switch (Args['T'].Value)
+                {
+                    case 1: return $"set audio pitch to {GetArg('S')}";
+                    case 4: return $"set audio count to {GetArg('S')}";
+                    case 7: return $"set audio delay to {GetArg('S')}";
+                    case 12: return $"set audio decay rate to {GetArg('S')}";
+                    default: return string.Empty;
+                }
+            }
+        }
     }
 }
--- Crash/Formats/Crash Formats/GOOL/GOOL Instructions/Sndp.cs
+++ Crash/Formats/Crash Formats/GOOL/GOOL Instructions/Sndp.cs
@@ -1,17 +1,17 @@
 ﻿namespace Crash.GOOLIns
 {
-    [GOOLInstruction(141,GameVersion.Crash1)]
-    [GOOLInstruction(141,GameVersion.Crash1Beta1995)]
-    [GOOLInstruction(141,GameVersion.Crash1BetaMAR08)]
-    [GOOLInstruction(141,GameVersion.Crash1BetaMAY11)]
-    [GOOLInstruction(66,GameVersion.Crash2)]
-    [GOOLInstruction(66,GameVersion.Crash3)]
+    [GOOLInstruction(140,GameVersion.Crash1)]
+    [GOOLInstruction(140,GameVersion.Crash1Beta1995)]
+    [GOOLInstruction(140,GameVersion.Crash1BetaMAR08)]
+    [GOOLInstruction(140,GameVersion.Crash1BetaMAY11)]
+    [GOOLInstruction(65,GameVersion.Crash2)]
+    [GOOLInstruction(65,GameVersion.Crash3)]
     public sealed class Sndp : GOOLInstruction
     {
         public Sndp(int value,GOOLEntry gool) : base(value,gool) { }
 
         public override string Name => "SNDP";
-        public override string Format => "[SSSSSSSSSSSS] (RRRRRR) FF VVVV";
-        public override string Comment => string.Empty;
+        public override string Format => DefaultFormat;
+        public override string Comment => $"play sound {GetArg('A')} at {GetArg('B')} volume";
     }
 }
