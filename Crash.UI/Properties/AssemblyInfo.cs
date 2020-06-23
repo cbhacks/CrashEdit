b05350cb6796c8e2dc264cc37a2ab83d0c0ea62a
chekwob <chek@wobbyworks.com>
2020-06-23 01:07:47 +0000

    Bump version number

--- Crash.UI/Properties/AssemblyInfo.cs
+++ Crash.UI/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.147")]
-[assembly: AssemblyFileVersion("0.2.147")]
+[assembly: AssemblyVersion("0.2.148")]
+[assembly: AssemblyFileVersion("0.2.148")]
--- CrashEdit/Properties/AssemblyInfo.cs
+++ CrashEdit/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.147")]
-[assembly: AssemblyFileVersion("0.2.147")]
+[assembly: AssemblyVersion("0.2.148")]
+[assembly: AssemblyFileVersion("0.2.148")]
 [assembly: NeutralResourcesLanguage("en")]
 
