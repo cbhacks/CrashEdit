b1730f704402393d41438db02ca51f30f91a9730
chekwob <chek@wobbyworks.com>
2021-09-10 03:53:52 +0000

    Add new hex editor

---
+++ Core/ByteArrayHexEditor.cs
@@ -0,0 +1,44 @@
+#nullable enable
+
+using System;
+using System.Windows.Forms;
+
+namespace CrashEdit {
+
+    public sealed class ByteArrayHexEditor : Editor {
+
+        public override string Text => "Hex";
+
+        public override bool ApplicableForSubject(Controller subj) {
+            if (subj == null)
+                throw new ArgumentNullException();
+
+            return subj.Resource is byte[];
+        }
+
+        protected override Control MakeControl() {
+            return new HexView {
+                Data = (byte[])Subject.Resource,
+                DataChangeHandler = HexView_DataChangeHandler,
+            };
+        }
+
+        private bool HexView_DataChangeHandler(int destOffset, int destLength, byte[] source) {
+            var data = (byte[])Subject.Resource;
+
+            if (destLength != source.Length)
+                throw new ArgumentException();
+            if (destOffset < 0 || destOffset >= data.Length)
+                throw new ArgumentException();
+
+            Array.Copy(source, 0, data, destOffset, destLength);
+            return true;
+        }
+
+        public override void Sync() {
+            ((HexView)Control).Invalidate();
+        }
+
+    }
+
+}
